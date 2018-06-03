04295cf24850ee9a83b654d0bb0de4a49495ab45
gauravsinghaec <gauravsinghaec@gmail.com>
2018-06-04 00:36:00 +0530

    Add Game functionality.

--- README.md
+++ README.md
@@ -1,20 +1,82 @@
 # Memory Game Project
+The Memory Game Project is all about demonstrating your mastery of HTML, CSS, and JavaScript. You’ll build a complete browser-based card matching game (also known as Concentration). But this isn’t just any memory game! It’s a shnazzy, well-designed, feature-packed memory game!
 
 ## Table of Contents
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+* [How The Game Works](#how+the+game+works)
+* [Working GitHub page](#instructions)
+* [Learn About Folder Structure](#learn+about+folder+structure)
+* [Prerequisites](#prerequisites)
+* [How to launch the app locally](#how+to+launch+the+app+locally)
+* [Authors](#authors)
+* [Acknowledgments](#acknowledgments)
 
-## Instructions
+## How The Game Works
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Each turn:
 
-To get started, open `js/app.js` and start building out the app's functionality
+- The player flips one card over to reveal its underlying symbol.
+- The player then turns over a second card, trying to find the corresponding card with the same symbol.
+- If the cards match, both cards stay flipped over.
+- If the cards do not match, both cards are flipped face down.
+- The game ends once all cards have been correctly matched.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+Game Displays below stats
+- Star Rating: The game displays a star rating (from 1 to at least 3) that reflects the player's performance. At the beginning of a game, it displays at least 3 stars. After 10 number of moves, it changes to a lower star rating(down to 2). After a few more moves(from 17 and above), it changes to a even lower star rating (down to 1).
+- Moves: Displays the current number of moves a user has made.
+- Time: Displays the time elapsed.
+- Restart: User can restart game any time.
+- Congratulations Popup: When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It also tell the user how much time it took to win the game, and what the star rating was.
 
-## Contributing
+## Learn About Folder Structure
+```
+Note : The folder structure may changes i.e we may include/exclude some folders/files
+as project progresses but the overall sructure will remain as presented below:
+```
+* index.html 	-- Project main file
+* css
+  - app.css   	-- CSS for our project
+* img  			-- Images for project
+* scss
+  - app.scss 	-- CSS preprocessor
+* js
+  - app.js   	-- CSS for our project
+* gulpfile.js  	-- This is a task runner to launch app and monitor for file(html,scss,css,js) changes.
+* package.json -- Project dev dependencies (for Grunt and Gulp)
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+[What is gulp?](https://gulpjs.com/)
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## Prerequisites
+* HTML5, CSS3, JavaScript, DOM, ES6
+
+## How to launch the app locally?
+* Step1 -- Fork the project repo and clone it in your local directory
+* Step2 -- Open the index.html in your browser and start playing the game.
+
+### Installation (for running using gulp and browsersync)
+* Step1 -- Install the node and npm
+
+* Step2 -- Install the npm modules from the package.json
+```
+>>> npm install
+this command installs all the node related packages required to run the app locally in
+/node_modules folder. You can see this folder inside /FEND_portfolio folder after running npm install
+```
+
+* Step3 -- Launch the application using below command:
+```
+Below command will run gulpfile.js and start the static project locally
+>>> npm install -g gulp
+>>> gulp
+The application will be running at http://localhost:3000 URL
+```
+
+
+## Authors
+* **Gaurav Singh**
+
+## Acknowledgments
+* **Special thanks to Udacity Team**
+
--- js/app.js
+++ js/app.js
@@ -1,19 +1,260 @@
-/*
- * Create a list that holds all of your cards
+/*jshint esversion: 6 */
+
+const openCardList = [];
+const totalCards = document.getElementsByClassName('card');
+const matchedCards = document.getElementsByClassName('card match');
+
+//************************
+/* *******Model
+ * Static list that holds all of the cards
+ * This list will be used to create cards on deck on page load
  */
+//************************
+let model = {
+    moveCounter: 0,
+    starCounter: 3,
+    cards : [
+    	'fa fa-diamond'
+        ,'fa fa-paper-plane-o'
+        ,'fa fa-anchor'
+        ,'fa fa-bolt'
+        ,'fa fa-cube'
+        ,'fa fa-anchor'
+        ,'fa fa-leaf'
+        ,'fa fa-bicycle'
+        ,'fa fa-diamond'
+        ,'fa fa-bomb'
+        ,'fa fa-leaf'
+        ,'fa fa-bomb'
+        ,'fa fa-bolt'
+        ,'fa fa-bicycle'
+        ,'fa fa-paper-plane-o'
+        ,'fa fa-cube'
+    ]
+};
 
+//************************
+// *******Controller
+//************************
+const controller = {
+    // Get the shuffled list od cards
+    getAllCards: function(){
+        return shuffle(model.cards);
+    },
+
+    // Get the user's start rating
+    getStar: function(){
+        return model.starCounter;
+    },
+
+    // Get the user's moves data
+    getMove: function(){
+        return model.moveCounter;
+    },
+
+    // Update user's moves data
+    updateMove: function(){
+        model.moveCounter++;
+        deckHeaderView.render();
+    },
+
+    // Update user's star data
+    updateStar: function(){
+    	if(model.moveCounter >10 && model.moveCounter <=16){
+    		model.starCounter = 2;
+    	}else if(model.moveCounter >16){
+    		model.starCounter = 1;
+    	}
+        deckHeaderView.render();
+    },
+    init: function(){
+        deckHeaderView.init();
+        cardListView.init();
+    },
+
+};
+//************************
+// *******Views
+//************************
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
+ * This view keeps track of move counter and star rating update
+ */
+let deckHeaderView = {
+    init: function(){
+        this.move = document.getElementsByClassName('moves')[0];
+        this.starList = document.getElementsByClassName('stars')[0];
+		this.restartBtn = document.getElementsByClassName('restart')[0];
+        this.render();
+    },
+
+    render: function(){
+        this.starList.textContent='';
+        this.move.textContent='';
+        const stars = controller.getAllCards();
+        const fragment = document.createDocumentFragment();
+        for(let i=1 ; i<=model.starCounter ;i++)
+        {
+            let elem    = document.createElement('li');
+            let starIcon = document.createElement('i');
+            starIcon.className = 'fa fa-star';
+            elem.appendChild(starIcon);
+            fragment.appendChild(elem);
+        }
+		this.starList.appendChild(fragment);//reflow and repaint here -- once!
+		this.move.textContent = controller.getMove();
+
+		this.restartBtn.addEventListener('click',restartGame,false);
+    }
+};
+
+/*
+ * This view display a message with the final score when all cards match
+ */
+let modalPopupView = {
+    init: function(){
+		this.modal = document.getElementById('winning-modal');
+		// Get the <span> element that closes the modal
+		this.modalSpan = document.getElementsByClassName('close')[0];
+		this.modalStar = document.getElementById('playStar');
+		this.modalMoves = document.getElementById('playMoves');
+		this.modalTime = document.getElementById('playerTime');
+		this.gameTime = document.getElementsByClassName('time')[0];
+		this.replayBtn = document.getElementById('replay');
+        this.render();
+    },
+
+    render: function(){
+		this.modalStar.textContent = controller.getStar();
+		this.modalMoves.textContent = controller.getMove();
+		this.modal.style.display = 'block';
+		this.modalTime.textContent = this.gameTime.textContent;
+		this.replayBtn.addEventListener('click',restartGame,false);
+
+		// When the user clicks on <span> (x), close the modal
+		this.modalSpan.addEventListener('click',function() {
+			let modal = document.getElementById('winning-modal');
+		    modal.style.display = 'none';
+		},false);
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.addEventListener('click',function(event) {
+			let modal = document.getElementById('winning-modal');
+		    if (event.target == modal) {
+		        modal.style.display = 'none';
+		    }
+		},false);
+    }
+};
+
+/*
+ * This view display the cards on the page
+ *   - shuffles the list of cards using the provided 'shuffle' method
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+let cardListView = {
+    init: function(){
+        this.cardList = document.getElementsByClassName('deck')[0];
+        this.cardDeck = document.getElementById('deck-board');
+        this.render();
+    },
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+    render: function(){
+        this.cardList.textContent='';
+        const cards = controller.getAllCards();
+        const fragment = document.createDocumentFragment();
+        for(const card of cards)
+        {
+            let elem    = document.createElement('li');
+            let cardIcon = document.createElement('i');
+            elem.className = 'card';
+            cardIcon.className = card;
+            elem.appendChild(cardIcon);
+            fragment.appendChild(elem);
+        }
+		this.cardList.appendChild(fragment);//reflow and repaint here -- once!
+		/*
+		 * set up the event listener for a card. If a card is clicked:
+		 */
+		this.cardDeck.addEventListener('click',
+			function(event) {
+				/*
+				 * Check if the target is fa-icon then take the parent
+				 * card as current card.
+				 */
+				const curr_card = (event.target.classList[0] === 'card')
+									? event.target
+									: event.target.parentElement;
+				if(curr_card.classList[1] === 'match'){
+				/*
+				 * Check if user clicks the matched card, if so
+				 * the don't trigger the default card click event
+				 */
+					event.preventDefault();
+					return;
+				}else if(curr_card.classList[1] === 'mismatch'){
+				/*
+				 * Remove the mismatch class to avoid conflict with rotate
+				 * animation when users flips the card by clicking on one of
+				 * the mismatched card.
+				 */
+					let mismatchCardList
+						= document.querySelectorAll('.mismatch');
+					for(const card of mismatchCardList){
+						card.classList.remove('mismatch');
+					}
+				}
+				// display the card's symbol ( using global unhide function)
+				unhideCard(curr_card);
+				if(openCardList.length === 0){
+				/*
+				 * if the *list* of 'open' cards is empty,
+				 * add the card to a *list* of 'open' cards
+				 */
+					openCardList.push(curr_card);
+				}else{
+				/*
+				 * if the list already has another card, check to see if
+				 * the two cards match
+				 */
+					const prev_card = openCardList.pop();
+					//update moves and start on page
+					controller.updateMove();
+					controller.updateStar();
+					if(prev_card && (prev_card.firstElementChild.classList[1]
+						=== curr_card.firstElementChild.classList[1]))
+					{
+					/*
+					 * if the cards do match,lock the cards in the open position
+					 */
+						matchCards(curr_card,prev_card);
+					}else{
+					/*
+					 * if the cards do not match, remove the cards from the
+					 * list and hide the card's symbol
+					 */
+						mismatchCards(curr_card,prev_card);
+					}
+				}
+			},false);
+    }
+};
+
+
+//******************************//
+//********Global Methods
+//******************************//
+
+/*
+ * Shuffle function from http://stackoverflow.com/a/2450976
+ * @param:
+ * 		array (data type: array): list of cards
+ * @returns:
+ * 		array (data type: array): shuffled list of cards
+ */
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
+    let currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -21,18 +262,89 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+/*
+ * Reload the page and start new game
+ * @param:
+ * 		None
+ * @returns:
+ * 		None
+ */
+function restartGame() {
+	document.location.reload();
+}
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Chack if all cards match,
+ * Stop the timer and
+ * display a message with the final score
+ * @param:
+ * 		None
+ * @returns:
+ * 		None
  */
+ function checkGameStatus() {
+	if (matchedCards.length == totalCards.length){
+		window.clearInterval(t);
+		modalPopupView.init();
+	}
+}
+
+/*
+ * Show the card's symbol
+ * @param:
+ * 		card (data type: array): event target card
+ * @returns:
+ * 		None
+ */
+function unhideCard(card) {
+    card.classList.add('open', 'show');
+}
+
+/*
+ * if the cards do match, lock the cards in the open position
+ * and add match class for animation effect
+ * @param:
+ * 		...cards (data type: array): two cards of a given move
+ * @returns:
+ * 		None
+ */
+function matchCards(...cards) {
+	for(const card of cards){
+		card.classList.remove('open', 'show');
+		card.classList.add('match');
+	}
+	checkGameStatus();
+}
+
+/*
+ * if the cards do not match, hide the card's symbol
+ * and add mismatch class for animation effect
+ * @param:
+ * 		...cards (data type: array): two cards of a given move
+ * @returns:
+ * 		None
+ */
+function mismatchCards(...cards) {
+	for(const card of cards){
+		card.classList.remove('open', 'show');
+		card.classList.add('mismatch');
+	}
+}
+
+/*
+ * Load the game page views once DOM is loaded
+ */
+window.addEventListener('DOMContentLoaded',controller.init(),false);
+
+/*
+ * increment the timer by 1 sec at each sec of interval
+ * use 't' to clear the time interval event when user wins
+ */
+let t = setInterval(function() {
+	let x = document.getElementsByClassName('time')[0].textContent;
+	document.getElementsByClassName('time')[0].textContent
+			= (parseInt(x, 10) + 1);
+}, 1000);
