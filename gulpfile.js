ddf64fdc2bff394532772124f6508faa4b23d4d9
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-27 18:36:29 +0530

    Remove unwanted code.

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,6 @@ gulp.task('sass',function () {
         .pipe(browserSync.stream());
 });
 
-// Move the Javascript files to public/assets/js
-gulp.task('js',function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest('public/assets/js'))
-        .pipe(browserSync.stream());
-});
-
 // Static Servers + watching html/scss files
 gulp.task('serve',function () {
     browserSync.init({
